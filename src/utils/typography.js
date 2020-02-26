a70dab5bf4c0547bc0935d22f38115deb9888e3a
Ed D. Chu <echu916@gmail.com>
2020-02-25 22:14:31 -0800

    Typeface changes

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,22 +1,31 @@
 import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import noriegaTheme from 'typography-theme-noriega'
 
-Wordpress2016.overrideThemeStyles = () => {
-  return {
-    "a.gatsby-resp-image-link": {
-      boxShadow: `none`,
-    },
-  }
-}
+noriegaTheme.baseLineHeight =  1.666;
+noriegaTheme.headerFontFamily = ['Avenir Next', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'];
+noriegaTheme.baseFontSize = '18px';
 
-delete Wordpress2016.googleFonts
+const typography = new Typography(
+  noriegaTheme
+);
+//import Wordpress2016 from "typography-theme-wordpress-2016"
 
-const typography = new Typography(Wordpress2016)
+//Wordpress2016.overrideThemeStyles = () => {
+//  return {
+//    "a.gatsby-resp-image-link": {
+//      boxShadow: `none`,
+//    },
+//  }
+//}
+
+//delete Wordpress2016.googleFonts
+
+//const typography = new Typography(Wordpress2016)
 
 // Hot reload typography in development.
-if (process.env.NODE_ENV !== `production`) {
-  typography.injectStyles()
-}
+//if (process.env.NODE_ENV !== `production`) {
+//  typography.injectStyles()
+//}
 
 export default typography
 export const rhythm = typography.rhythm
